f72acc7ec6eb6938e434d4836df3c2be00e6d1de
shahnewaz601 <shahnewaz601@gmail.com>
2021-04-29 19:02:02 +0600

    insert order info and fix nodemon

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const foodCollection = client.db("redOnionStore").collection("foods");
+  const orderCollection = client.db("redOnionStore").collection("orders");
     
     //insert foods
     app.post('/addFoods', (req, res) => {
@@ -46,6 +47,31 @@ client.connect(err => {
         .toArray( (err, documents) => {
             res.send(documents[0]);
         })
+    })
+
+    //insert order data
+    app.post('/addOrder', (req, res) => {
+        const order = req.body;
+        orderCollection.insertOne(order)
+        .then(result => {
+        console.log(result.insertedCount);
+        res.send(result.insertedCount > 0);
+        })
+    })
+
+     //read specific user booking data
+     app.post('/placeOrder', (req, res) => {
+        const orderDetails = req.body;
+        console.log(orderDetails);
+        orderCollection.insertOne(orderDetails, (err, result) => {
+            if(err){
+                console.log(err)
+                res.status(500).send({message: err});
+            }
+            else{
+                res.send(result.ops[0]);
+            }
+        });
     })
 
 
